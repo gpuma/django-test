a6f05a05b9ae60cd20a489b94d7c642ace714587
Gustavo Puma <vash47@gmail.com>
2018-01-02 20:45:35 -0500

    word frequency done

--- word_cloud.py
+++ word_cloud.py
@@ -1,19 +1,50 @@
 # nlp library
-from nltk import tokenize
 from nltk.corpus import stopwords
 # for command line arguments
 import sys
+import re
+
 
 def get_words(filename):
+    """Returns a list of words extracted from the file"""
+    # words are found by matching against a regex
+    # then they're further filtered by excluding any
+    # english stop words, according to nltk data
+    # todo: remove pronouns
+    # lastly, they're converted to lowercase
     file = open(filename)
     raw = file.read()
     file.close()
-    # tokenizes using english by default
-    words = tokenize.word_tokenize(raw)
-    filtered_words = [w for w in words if w not in stopwords.words('english')]
+    words = re.findall(r'\w+', raw)
+
+    # we cast the stopwords into a set to speed it up, since sets
+    # are implemented as hash tables
+    filtered_words = [w.lower() for w in words if w not in set(stopwords.words('english'))]
     return filtered_words
+
+
+def get_word_freq(word_list, normalize=True):
+    """Returns a sorted list of (word,word count) tuples in descending order.
+    The frequency is normalized to values between 0 and 1 by default."""
+    word_freq_dict = {}
+    for w in word_list:
+        if w in word_freq_dict:
+            word_freq_dict[w] += 1
+        else:
+            word_freq_dict[w] = 1
+    if normalize:
+        # inplace update to avoid recreating the dictionary again
+        word_freq_dict.update((key, round(val / len(word_list), 3)) for key, val in word_freq_dict.items())
+
+    unsorted_word_freq = [(key, val) for key, val in word_freq_dict.items()]
+    # sorting by word frequency in descending order
+    word_freq = sorted(unsorted_word_freq, key=lambda tup: tup[1], reverse=True)
+    return word_freq
+
 
 filename = sys.argv[1]
 words = get_words(filename)
-print (words)
-print("there's %d words" % len(words))
+#print (words)
+#print("there's %d words" % len(words))
+#print("this is the frequency")
+print(get_word_freq(words)[:10])
