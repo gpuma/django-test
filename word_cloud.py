8f6f82639f023179366a99a6354ca95fe1af2db0
Gustavo Puma <vash47@gmail.com>
2018-01-05 23:02:10 -0500

    refactoring to classes

--- word_cloud.py
+++ word_cloud.py
@@ -6,6 +6,62 @@ from PIL import Image, ImageDraw, ImageColor, ImageFont
 import sys
 import re
 
+class WordCloud:
+    def __init__(self, word_freq, x, y, max_font_size, background="white", foreground="black"):
+        self.max_font_size = max_font_size
+        self.canv_x = x
+        self.canv_y = y
+        self.bg_color = background
+        self.fg_color = foreground
+        self.img = Image.new("RGB", (self.canv_x, self.canv_y), ImageColor.getrgb(background))
+        self.draw = ImageDraw.Draw(self.img)
+        #self.fnt_location = "/usr/share/fonts/truetype/dejavu/DejaVuSerif.ttf"
+        self.font_location = "C:\\Anaconda3\\Library\\lib\\fonts\\DejaVuSerif.ttf"
+        #ImageFont.truetype(font_location, font_size)
+        # todo: uncomment
+        #self.words = [Word(w, f) for w, f in word_freq]
+
+    def get_center_position(self, word):
+        """given a text of type Word, it returns the coordinates at which it
+        should be drawn on the current canvas, in order to appear in the center."""
+        # todo: it's not actually the center, it's a bit off
+        return self.canv_x / 2 - (word.length * word.font_size) / 2, self.canv_y / 2 - word.font_size / 2
+
+    def draw_word(self, word):
+        """draws a specific Word onto the canvas"""
+        fnt = ImageFont.truetype(self.font_location, word.font_size)
+        # todo: draw in actual position instead of center
+        word.x, word.y = self.get_center_position(word)
+        self.draw.text((word.x, word.y), word.text, font=fnt, fill=ImageColor.getrgb(self.fg_color))
+        self.draw.rectangle(word.get_rectangle(), outline=self.fg_color)
+
+    def show_word_cloud(self):
+        """generates an image with the words drawn over it"""
+        self.img.show()
+
+
+class Word:
+    def __init__(self, word, freq, font_size=0):
+        self.text = word
+        self.length = len(word)
+        self.freq = freq
+        self.font_size = font_size
+        self.x = -1
+        self.y = -1
+        self.box_x = -1
+        self.box_y = -1
+
+    def get_rectangle(self):
+        # calculates the four coordinates required to draw a rectangle around the text in order to detect collisions
+        # let's break it down: at first I thought the x coordinates of the rectangles would be equal to
+        # fontsize * number of letters. Turns out that's too big and that diving the length in half
+        # gives a decent result. however, the last letter is cut off so, in order to avoid that
+        # I add 1 (it sometimes leaves an empty space). I was going with 0.5, which actually creates a tight fit,
+        # but it cuts off the last letter in some cases.
+        # For the y coordinates, just adding the font_size worked fine except for letter like 'p' and 'q', which
+        # have kind of like a tail. To accomodate for that, adding 0.25 to the font size works great.
+        return [self.x, self.y, self.x + self.font_size * (self.length // 2 + 1), self.y + self.font_size * 1.25]
+
 
 def get_words(filename):
     """Returns a list of words extracted from the file"""
@@ -26,7 +82,7 @@ def get_words(filename):
 
 
 def get_word_freq(word_list, normalize=True):
-    """Returns a sorted list of (word,word count) tuples in descending order.
+    """Returns a sorted list of (text,text count) tuples in descending order.
     The frequency is normalized to values between 0 and 1 by default."""
     word_freq_dict = {}
     for w in word_list:
@@ -39,39 +95,32 @@ def get_word_freq(word_list, normalize=True):
         word_freq_dict.update((key, round(val / len(word_list), 3)) for key, val in word_freq_dict.items())
 
     unsorted_word_freq = [(key, val) for key, val in word_freq_dict.items()]
-    # sorting by word frequency in descending order
+    # sorting by text frequency in descending order
     word_freq = sorted(unsorted_word_freq, key=lambda tup: tup[1], reverse=True)
     return word_freq
 
 
-def create_word_cloud(word_list, width, height, font_size, background, foreground, xy):
-    word = "benis"
-    img = Image.new("RGB", (width, height), ImageColor.getrgb(background))
-    fnt = ImageFont.truetype(font_location, font_size)
-    draw = ImageDraw.Draw(img)
-    draw.text(xy, word, font=fnt, fill=ImageColor.getrgb(foreground))
-    # create a rectangle around the word in order to detect collisions
-    # let's break it down: at first I thought the x coordinates of the rectangles would be equal to
-    # fontsize * number of letters. Turns out that's too big and that diving the length in half gives a decent result
-    # however, the last letter is cut off, so to avoid that I add 1 (it sometimes leaves an empty space). I was going
-    # with 0.5, which actually creates a tight fit, but it cuts off the last letter in some cases.
-    # For the y coordinates, just adding the font_size worked fine except for letter like 'p' and 'q', which
-    # have kind of like a tail. To accomodate for that, adding 0.25 to the font size works great.
-    draw.rectangle([xy, (xy[0]+font_size*(len(word)//2+1), xy[1]+font_size*1.25)], outline=foreground)
-    img.show()
+# w[0]-> text, w[1] -> frequency, w[2] -> coordinates, w[3] -> coordinates
+def place_words(word_list, max_font, width, height):
+    max_freq=word_list[0][1]
+    word_list[0][2]=get_center_position(width, height, word_list[0])
+    #for i in range(1,len(word_list) - 1)
 
 # todo: put this in a config file
-font_location = "/usr/share/fonts/truetype/dejavu/DejaVuSerif.ttf"
 width = int(sys.argv[1])
 height = int(sys.argv[2])
 fnt_size = int(sys.argv[3])
-bg_color = sys.argv[4]
-fg_color = sys.argv[5]
-xy = (int(sys.argv[6]), int(sys.argv[7]))
+#bg_color = sys.argv[4]
+#fg_color = sys.argv[5]
+#xy = (int(sys.argv[6]), int(sys.argv[7]))
 
 #words = get_words(filename)
 #print (words)
 #print("there's %d words" % len(words))
 #print("this is the frequency")
 #print(get_word_freq(words)[:10])
-create_word_cloud(None, width, height, fnt_size, bg_color, fg_color, xy)
+
+wc = WordCloud(None, width, height, fnt_size)
+wc.draw_word(Word("caquita", 0, fnt_size))
+wc.show_word_cloud()
+#create_word_cloud(None, width, height, fnt_size, bg_color, fg_color, xy)
