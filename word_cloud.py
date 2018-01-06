5f9e70a1eb8d53415900c7863719b3a94948ee43
Gustavo Puma <vash47@gmail.com>
2018-01-06 11:56:22 -0500

    collision algorithm ready

--- word_cloud.py
+++ word_cloud.py
@@ -6,6 +6,7 @@ from PIL import Image, ImageDraw, ImageColor, ImageFont
 import sys
 import re
 
+
 class WordCloud:
     def __init__(self, word_freq, x, y, max_font_size, background="white", foreground="black"):
         self.max_font_size = max_font_size
@@ -31,9 +32,9 @@ class WordCloud:
         """draws a specific Word onto the canvas"""
         fnt = ImageFont.truetype(self.font_location, word.font_size)
         # todo: draw in actual position instead of center
-        word.x, word.y = self.get_center_position(word)
+        # word.x, word.y = self.get_center_position(word)
         self.draw.text((word.x, word.y), word.text, font=fnt, fill=ImageColor.getrgb(self.fg_color))
-        self.draw.rectangle(word.get_rectangle(), outline=self.fg_color)
+        self.draw.rectangle(word.get_box_coord(), outline=self.fg_color)
 
     def show_word_cloud(self):
         """generates an image with the words drawn over it"""
@@ -41,18 +42,22 @@ class WordCloud:
 
 
 class Word:
-    def __init__(self, word, freq, font_size=0):
+    def __init__(self, word, freq, x, y, font_size=0):
         self.text = word
         self.length = len(word)
         self.freq = freq
         self.font_size = font_size
-        self.x = -1
-        self.y = -1
-        self.box_x = -1
-        self.box_y = -1
+        self.x = x
+        self.y = y
+        # has the form: [x0, y0, x1, y1] (from PIL.ImageDraw)
+        self.box = self.get_box_coord()
 
-    def get_rectangle(self):
-        # calculates the four coordinates required to draw a rectangle around the word in order to detect collisions
+    def get_box_dimensions(self):
+        """returns the width and height of the box around the word"""
+        return self.box[2] - self.box[0], self.box[3] - self.box[1]
+
+    def get_box_coord(self):
+        # calculates the two coordinates required to draw a rectangle around the word in order to detect collisions
         # let's break it down: at first I thought the x coordinates of the rectangles would be equal to
         # fontsize * number of letters. Turns out that's too big and that diving the length in half
         # gives a decent result. however, the last letter is cut off so, in order to avoid that
@@ -61,6 +66,24 @@ class Word:
         # For the y coordinates, just adding the font_size worked fine except for letter like 'p' and 'q', which
         # have kind of like a tail. To accomodate for that, adding 0.25 to the font size works great.
         return [self.x, self.y, self.x + self.font_size * (self.length // 2 + 1), self.y + self.font_size * 1.25]
+
+    def get_center_coord(self):
+        """returns the center point of the box"""
+        # midpoint formula
+        return (self.box[0] + self.box[2]) * 0.5, (self.box[1] + self.box[3]) * 0.5
+
+    def collides(self, other_word):
+        """returns True if the box around this word overlaps with the box of the other word."""
+        (width_box_a, height_box_a), (width_box_b, height_box_b) = self.get_box_dimensions(), other_word.get_box_dimensions()
+        # distance between the center points of both boxes
+        length = abs(self.get_center_coord()[0] - other_word.get_center_coord()[0])
+        height = abs(self.get_center_coord()[1] - other_word.get_center_coord()[1])
+        gap_x = length - width_box_a * 0.5 - width_box_b * 0.5
+        gap_y = height - height_box_a * 0.5 - height_box_b * 0.5
+
+        # a negative gap in both axis indicates that the boxes are overlapping
+        # not considering when the edges are just touching
+        return gap_x < 0 and gap_y < 0
 
 
 def get_words(filename):
@@ -121,6 +144,10 @@ fnt_size = int(sys.argv[3])
 #print(get_word_freq(words)[:10])
 
 wc = WordCloud(None, width, height, fnt_size)
-wc.draw_word(Word("caquita", 0, fnt_size))
+word_a = Word("caquita", 0, 40, 50, fnt_size)
+word_b = Word("penis", 0, 350, 50, fnt_size)
+wc.draw_word(word_a)
+wc.draw_word(word_b)
 wc.show_word_cloud()
+print(word_a.collides(word_b))
 #create_word_cloud(None, width, height, fnt_size, bg_color, fg_color, xy)
