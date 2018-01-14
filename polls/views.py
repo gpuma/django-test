4fe09e72849560ca81085b2067839c8345bbb6be
Gustavo Puma <vash47@gmail.com>
2018-01-14 16:39:23 -0500

    before generic views

--- polls/views.py
+++ polls/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 
-from .models import Question
+from .models import Question, Choice
 
 
 def index(request):
@@ -17,8 +17,8 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    response = "Estos son los resultados de la pregunta %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 
 def vote(request, question_id):
@@ -27,10 +27,11 @@ def vote(request, question_id):
         selected_choice = q.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
         return render(request, 'polls/detail.html', {
-            'question': question,
+            'question': q,
             'error_message': "No seleccionaste una opción.",
         })
     else:
         selected_choice.votes += 1
         selected_choice.save()
-        return HttpResponseRedirect(reverse('polls:results',args=(question.id,)))
+        # reverse avoids having to hardcode a url
+        return HttpResponseRedirect(reverse('polls:results', args=(q.id,)))
