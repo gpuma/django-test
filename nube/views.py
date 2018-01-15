9f04a8e0dad3ccbc466e76ff11dd35f299d7523d
Gustavo Puma <vash47@gmail.com>
2018-01-15 18:30:19 -0500

    some boilerplate done

--- nube/views.py
+++ nube/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+#class IndexView(generic.FormView):
+    #template_name = 'nube/index.html'
+
+def index(request):
+    # todo: turn this into a generic view?
+    return render(request, 'nube/index.html')
+
+def create(request):
+    # todo: pending
+    return render(request, None)
--- phuyu/urls.py
+++ phuyu/urls.py
@@ -2,6 +2,7 @@ from django.urls import include, path
 from django.contrib import admin
 
 urlpatterns = [
+    path('nube/', include('nube.urls')),
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
